7345bb569aac4aa4decf381bd1f9c09996b61d4a
Rahaman <rkazembe023@student.wethinkcode.co.za>
2025-01-16 20:06:26 +0200

    completed authentication routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import cloudinray from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
 
@@ -47,9 +48,70 @@ export const signup = async (req, res) => {
         
     }
 }
-export const login = (req, res) => {
-    res.send("Signup page")
+
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if(!email || !password) {
+           return res.status(400).json({message : "Fields must not be empty"})
+        }
+        const user = await User.findOne({email});
+        if(!user) {
+           return res.status(400).json({message: "Invalid Credentials"});
+        } else {
+            const correctPassword = await bcrypt.compare(password, user.password);
+            if (!correctPassword) {
+               return res.status(400).json({message: "Invalid Credentials"});
+            }
+            generateToken(user._id, res);
+            const  {_id, fullName, email, profilePic } = user
+           return res.status(200).json({_id, fullName, email, profilePic})
+        }
+        
+    } catch (error) {
+        res.status(500).json({message :"Interal Server Error"})
+    }
+    
 }
+
 export const logout = (req, res) => {
-    res.send("Signup page")
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message : "Log out success"})
+    } catch (error) {
+        console.log("Error in logout controller\n", error);
+        res.status(500).json({message :"Interal Server Error"})
+        
+    }
+}
+
+
+export const updateProfile = async ( req, res ) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id; // set in the protect route function
+        if(!profilePic) {
+            res.status(400).json({message :"Profile picture is required"})
+        }
+
+        const uploadResponse = await cloudinray.uploader.upload(profilePic);
+        const updatedUser = User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("An error occured in the updateProfile function", error);
+        res.status(500).json({message: "Interal Server Error"});
+    }
+}
+
+
+export const checkAuth =  (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log( "Error in the checkAuth Controller" ,error);
+        res.status(500).json({message: "Interal Server Error"});
+        
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
 
 
 dotenv.config();
@@ -10,6 +12,7 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json()); // allows you to extract the data from the json body
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
